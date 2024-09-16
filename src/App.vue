3e6e36cf43d2985779aea8ed1007719d0826820f
Kaleidiana <kaleidiana63@gmail.com>
2024-09-16 13:07:22 +0300

    Update code

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <NavComponent/>
+</template>
+
+<script>
+import NavComponent from './components/NavComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    NavComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
